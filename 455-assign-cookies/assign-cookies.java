a959cdc1aebe2a59ec6d07732b2f3566d09a0de9
Lingesh sathya narayana c m <125336607+lingeshsathyanarayanacm@users.noreply.github.com>
2025-02-16 14:00:41 +0530

    Time: 11 ms (42.10%) | Memory: 49 MB (15.07%) - LeetSync

---
+++ 455-assign-cookies/assign-cookies.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g); // Sort children's greed factor
+        Arrays.sort(s); // Sort cookie sizes
+
+        int i = 0, j = 0, count = 0;
+
+        while (i < g.length && j < s.length) {
+            if (s[j] >= g[i]) { // If the cookie can satisfy the child
+                count++;
+                i++; // Move to the next child
+            }
+            j++; // Always move to the next cookie
+        }
+
+        return count;
+    }
+}
