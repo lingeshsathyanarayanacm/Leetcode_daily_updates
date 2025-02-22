f8caddd4d50ae049bd8b6dc1507fb94d340bfd78
Lingesh sathya narayana c m <125336607+lingeshsathyanarayanacm@users.noreply.github.com>
2025-02-22 07:09:11 +0530

    Time: 5 ms (72.91%) | Memory: 42.2 MB (38.44%) - LeetSync

--- 2053-check-if-all-characters-have-equal-number-of-occurrences/check-if-all-characters-have-equal-number-of-occurrences.java
+++ 2053-check-if-all-characters-have-equal-number-of-occurrences/check-if-all-characters-have-equal-number-of-occurrences.java
@@ -1,16 +1,28 @@
+import java.util.*;
+
 class Solution {
     public boolean areOccurrencesEqual(String s) {
-        int firstCharCount = s.length() - s.replaceAll(String.valueOf(s.charAt(0)), "").length();
+        char ch[] = s.toCharArray();
+        List<Integer> freqList = new ArrayList<>();
+        List<Character> checkedChars = new ArrayList<>();
 
-        for (int i = 1; i < s.length(); i++) {
-            char currentChar = s.charAt(i);
-            if (currentChar != s.charAt(0)) {
-                int currentCharCount = s.length() - s.replaceAll(String.valueOf(currentChar), "").length();
-                if (currentCharCount != firstCharCount) {
-                    return false;
+        for (char c : ch) {
+            if (!checkedChars.contains(c)) {
+                int count = 0;
+                for (char x : ch) {
+                    if (c == x) count++;
                 }
+                freqList.add(count);
+                checkedChars.add(c);
             }
         }
+
+        for (int i = 1; i < freqList.size(); i++) {
+            if (!freqList.get(i).equals(freqList.get(0))) {
+                return false;
+            }
+        }
+
         return true;
     }
 }
