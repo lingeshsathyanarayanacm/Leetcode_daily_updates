241c167dd0ce54937e79bcdfcace93d37b30f42e
Lingesh sathya narayana c m <125336607+lingeshsathyanarayanacm@users.noreply.github.com>
2025-02-23 15:20:25 +0530

    Time: 4 ms (93.15%) | Memory: 41.3 MB (84.90%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.java
+++ 518-coin-change-ii/coin-change-ii.java
@@ -1,14 +1,18 @@
+import java.util.HashMap;
+import java.util.Map;
+
 class Solution {
     public int change(int amount, int[] coins) {
         int[] dp = new int[amount + 1];
-        dp[0] = 1;
-
-        for (int coin : coins) {
+        dp[0] = 1; 
+        
+        for (int i = 0; i < coins.length; i++) {
+            int coin = coins[i];
             for (int j = coin; j <= amount; j++) {
                 dp[j] += dp[j - coin];
             }
         }
-
+        
         return dp[amount];
     }
 }
