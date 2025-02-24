f490754be66b305aea8ca378dd3f3346911003f8
Lingesh sathya narayana c m <125336607+lingeshsathyanarayanacm@users.noreply.github.com>
2025-02-24 17:17:30 +0530

    Time: 0 ms (100.00%) | Memory: 41.6 MB (79.62%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.java
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -1,17 +1,24 @@
 class Solution {
-    private Integer[][] dp;
-
+    private int dp[][];
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-        int rows=obstacleGrid.length, cols=obstacleGrid[0].length;
-        if (obstacleGrid[0][0]==1||obstacleGrid[rows-1][cols-1]==1) return 0;
-        dp=new Integer[rows][cols]; 
+        int rows=obstacleGrid.length;
+        int cols=obstacleGrid[0].length;
+        dp=new int[rows][cols];
+        if(obstacleGrid[0][0]==1 || obstacleGrid[rows-1][cols-1]==1)return 0;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                dp[i][j] = -1;
+            }
+        }
         return dfs(obstacleGrid,0,0,rows,cols);
-    }
 
-    private int dfs(int[][] ob,int i,int j,int rows,int cols) {
-        if (i>=rows||j>=cols||ob[i][j]==1) return 0;
-        if (i==rows-1&&j==cols-1) return 1;
-        if (dp[i][j]!=null) return dp[i][j];
-        return dp[i][j]=dfs(ob,i+1,j,rows,cols) + dfs(ob, i,j+1,rows,cols);
+    }
+    public int dfs(int ob[][],int i,int j,int rows,int cols)
+    {
+        if(i>=rows||j>=cols||ob[i][j]==1)return 0;
+        if(i==rows-1 && j==cols-1) return 1;
+        if(dp[i][j]!=-1) return dp[i][j];
+        dp[i][j]=dfs(ob,i+1,j,rows,cols)+dfs(ob,i,j+1,rows,cols);
+        return dp[i][j];
     }
 }
