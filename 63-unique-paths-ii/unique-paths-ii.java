b863085f3505685aad985d4726de6900075357ae
Lingesh sathya narayana c m <125336607+lingeshsathyanarayanacm@users.noreply.github.com>
2025-02-24 12:02:10 +0530

    Time: 0 ms (100.00%) | Memory: 41.6 MB (79.62%) - LeetSync

---
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -0,0 +1,17 @@
+class Solution {
+    private Integer[][] dp;
+
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int rows = obstacleGrid.length, cols = obstacleGrid[0].length;
+        if (obstacleGrid[0][0] == 1 || obstacleGrid[rows - 1][cols - 1] == 1) return 0;
+        dp = new Integer[rows][cols]; 
+        return dfs(obstacleGrid, 0, 0, rows, cols);
+    }
+
+    private int dfs(int[][] ob, int i, int j, int rows, int cols) {
+        if (i >= rows || j >= cols || ob[i][j] == 1) return 0;
+        if (i == rows - 1 && j == cols - 1) return 1;
+        if (dp[i][j] != null) return dp[i][j];
+        return dp[i][j] = dfs(ob, i + 1, j, rows, cols) + dfs(ob, i, j + 1, rows, cols);
+    }
+}
