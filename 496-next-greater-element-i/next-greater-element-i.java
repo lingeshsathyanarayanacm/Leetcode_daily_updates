ddda45c18a2e2fc286e3b139d67d7d6c2ac2bb14
Lingesh sathya narayana c m <125336607+lingeshsathyanarayanacm@users.noreply.github.com>
2025-02-25 06:50:31 +0530

    Time: 2 ms (98.91%) | Memory: 44.4 MB (24.59%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -1,26 +1,22 @@
 class Solution {
-    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        int[] arr = new int[nums1.length];
-
-        for (int i = 0; i < nums1.length; i++) {
-            int element = nums1[i];
-            int nextGreater = -1;
-
-            for (int j = 0; j < nums2.length; j++) {
-                if (nums2[j] == nums1[i]) {
-
-                    for (int k = j + 1; k < nums2.length; k++) {
-                        if (nums2[k] >nums1[i]) {
-                            nextGreater = nums2[k];
-                            break;
-                        }
-                    }
-
-                    break; 
-                }
-            }
-            arr[i] = nextGreater; 
+     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        int[] mp = new int[10001];
+        Stack<Integer> s = new Stack<Integer>();
+        for(int i=0;i<nums2.length;i++){
+            while(!s.isEmpty() && s.peek() < nums2[i]){
+                mp[s.pop()] = nums2[i];
+            } 
+            s.push(nums2[i]); 
         }
-        return arr;
+        int ans = -1;
+        for(int i=0;i<nums1.length;i++){
+            ans = mp[nums1[i]];
+            if(ans==0){
+                nums1[i] = -1;
+            } else {
+                nums1[i]=ans;
+            }
+        }
+        return nums1;
     }
 }
